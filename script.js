91c2d2fbea0fb768aea5d1b8a62636f3dfd38cd5
Mohamed t c <97926192+hamdtc@users.noreply.github.com>
2023-03-16 11:48:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+let btn = document.getElementById("btn");
+let output = document.getElementById("output")
+
+btn.addEventListener('click', async ()=>{
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data,data.title);
+    output.innerHTML = data.title;
+})
+//your JS code here. If required.
